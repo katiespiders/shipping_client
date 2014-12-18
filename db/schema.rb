10b3f9562ce769c579e964ea5074fd573adb518a
Katie <kab761@gmail.com>
2014-12-18 12:23:55 -0800

    added dimensions array column to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211230410) do
+ActiveRecord::Schema.define(version: 20141218183706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20141211230410) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image"
+    t.integer  "dimensions",                              array: true
   end
 
   add_index "products", ["canonical_name"], name: "index_products_on_canonical_name", using: :btree
