c70e112ff57283b7fe37fe2a6bef496d5b59e88a
Katie <kab761@gmail.com>
2014-12-18 12:24:50 -0800

    started parsing of package and address data into query strings, weird bug with dimensions being double added to first item

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
+
 class Order < ActiveRecord::Base
   has_many :items, class_name: "LineItem"
   has_one :address
@@ -9,15 +10,80 @@ class Order < ActiveRecord::Base
     Money.new items.sum(:total_cents)
   end
 
-  private
+  def fedex_options
+    HTTParty.get("http://frozen-bastion-1170.herokuapp.com/shipments")
+  end
 
-  def set_number
-    while !self.number || Order.exists?(number: self.number.to_s)
-      self.number = create_number
+  #private
+
+    def set_number
+      while !self.number || Order.exists?(number: self.number.to_s)
+        self.number = create_number
+      end
     end
-  end
 
-  def create_number
-    (SecureRandom.random_number(9000000) + 1000000).to_s
-  end
+    def create_number
+      (SecureRandom.random_number(9000000) + 1000000).to_s
+    end
+
+    def to_query_string
+
+    end
+
+    def origin_query
+      { origin: {
+          country: 'US',
+          state:   'WA',
+          city:    'Seattle',
+          zip:     '98103'
+        }}.to_query
+    end
+
+    # def packages
+    #   @packages_array = []
+    #   items.each do |line_item|
+    #     item = Product.find(line_item.product_id)
+    #     line_item.quantity.times do
+    #       @packages_array << Package.new(item.weight, item.dimensions)
+    #     end
+    #   end
+    # end
+
+    def packages_query
+      packages_array = []
+      items.each do |line_item|
+        puts "!"*80, line_item.inspect
+        line_item.quantity.times do
+          item = Product.find(line_item.product_id)
+          packages_array << { weight: item.weight, dimensions: item.dimensions }
+        end
+      end
+      puts "@"*80, packages_array.inspect
+      packages_array.to_query(:packages)
+    end
+    #
+    # def destination
+    #   Location.new(
+    #     country: 'US',
+    #     zip: address.postal_code,
+    #     state: address.state,
+    #     city: address.city
+    #   )
+    # end
+
+    def destination_query
+      # state = address.state
+      # city = address.city.gsub!(" ", "%20")
+      # zip = address.postal_code
+      # "destination[country]=US&destination[state]=#{state}&destination[city]=#{city}&destination[zip]=#{zip}"
+      { destination: {
+        country:  'US',
+        state:    address.state,
+        city:     address.city,
+        zip:      address.postal_code
+        }}.to_query
+    end
+
+
+
 end
