6e220dd82d4dedec94f623750014191f5ac92c46
Katie <kab761@gmail.com>
2014-12-26 14:25:34 -0800

    fixed weird migration

--- db/migrate/20141218183706_add_dimensions_to_products.rb
+++ db/migrate/20141218183706_add_dimensions_to_products.rb
@@ -1,6 +1,5 @@
 class AddDimensionsToProducts < ActiveRecord::Migration
   def change
-    remove_column :products, :dimensions
     add_column :products, :dimensions, :integer, array: true
   end
 end
--- db/migrate/20141219233518_remove_logs_table.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLogsTable < ActiveRecord::Migration
-  def change
-    drop_table :logs
-  end
-end
